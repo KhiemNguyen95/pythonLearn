2541fa2f49fb0d55e399c7bf2ddcc8d8de159967
Khiem Nguyen <nkhiem5@gmail.com>
2018-04-03 23:42:11 +0700

    learn

--- basic.py
+++ basic.py
@@ -1,3 +1,20 @@
 print  ("khiem");
 a = 10
-
+def  sum (a, b) :
+    print a
+    print b
+    return (a+b)
+c = sum (2, 3);
+print c
+print "---------"
+# dictionary
+print "dictionary"
+point = {'x': 1,'y': 2}
+print point ['x']
+point['z'] = 3
+print point
+print point.has_key('z')
+print "---------"
+import random
+print random .__file__
+print dir ()
